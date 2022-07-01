c4cfe8ea836953b8fb72e60542832ad72f42999f
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2022-07-01 20:16:01 +0200

    changes

---
+++ hello/JavaScrips/alter.js
@@ -0,0 +1,18 @@
+console.clear();
+
+let name = "Rudi";
+let age = 65;
+
+let months = age * 12;
+let days = age * 365;
+let hours = days * 24;
+let minutes = hours * 60;
+let seconds = minutes * 60;
+
+console.log("Mein Name ist " + name + ".");
+console.log("Ich bin " + age + " Jahre alt");
+console.log("Ich bin " + months + " Monate alt.");
+console.log("Ich bin " + days + " Tage alt.");
+console.log("Ich bin " + hours + " Stunden alt.");
+console.log("Ich bin " + minutes + " Minuten alt.");
+console.log("Ich bin " + seconds + " Sekunden alt.");
---
+++ hello/JavaScrips/berechnungen.js
@@ -0,0 +1,17 @@
+console.clear();
+
+let a = 5;
+let b = 6;
+
+let result = a + b;
+
+console.log(result);
+
+
+const pi = 3.1415;
+
+let radius = 10;
+
+let umfang = pi * 2 * radius;
+
+console.log("Umfang ist", umfang)
