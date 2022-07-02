4c5ac6ba2c89f9fe8234051d0f9008ae451961e4
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2022-07-02 12:01:47 +0200

    primitive_datentypen

---
+++ hello/JavaScrips/primitive_datentyplen.js
@@ -0,0 +1,23 @@
+console.clear();
+
+// -------Primitive Datentypen
+let name = "Junus Ergin"; // String ist Variable
+let age = 29; // number
+let isStudend = false; // boolean ist binär 0 oder 1
+
+// Komplexe Datentypen
+let contact = {
+  firstName: "Junus",
+  lastName: "Ergin"
+  age: 30,
+  isStudend: false
+};
+
+let contact = {
+  firstName: "Hans",
+  lastName: "Mayer"
+  age: 86,
+  isStudend: false
+};
+
+console.log(contact.firstName + " " + contact2.lastName);
