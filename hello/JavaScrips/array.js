831bc335dd17d6f27b49347ac3ef05d993a8ec51
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2022-07-02 14:30:39 +0200

    changes

--- hello/JavaScrips/array.js
+++ hello/JavaScrips/array.js
@@ -5,9 +5,9 @@ let notebook = ["Eier kaufen", "Mehl kaufen", "Putzen", "Sport machen"];
 let newNote = prompt();
 notebook.push(newNote); // Zwischenschritt
 
-console.log("Notiz 1 ist" + notebook[0]);
-console.log("Notiz 1 ist" + notebook[1]);
-console.log("Notiz 1 ist" + notebook[2]);
-console.log("Notiz 1 ist" + notebook[3]);
-console.log("Notiz 1 ist" + notebook[4]);
-console.log("Notiz 1 ist" + notebook[5]);
+console.log("Notiz 1" + notebook[0]);
+console.log("Notiz 2" + notebook[1]);
+console.log("Notiz 3" + notebook[2]);
+console.log("Notiz 4" + notebook[3]);
+console.log("Notiz 5" + notebook[4]);
+console.log("Notiz 6" + notebook[5]);
