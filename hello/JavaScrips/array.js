9c85712aa83272206735b1eda153e5c66cd2f354
Rudi Ziebart <99054280+Skorpion115@users.noreply.github.com>
2022-07-02 12:41:11 +0200

    Create array.js

---
+++ hello/JavaScrips/array.js
@@ -0,0 +1,13 @@
+console.clear();
+// Array
+let notebook = ["Eier kaufen", "Mehl kaufen", "Putzen", "Sport machen"];
+
+let newNote = prompt();
+notebook.push(newNote); // Zwischenschritt
+
+console.log("Notiz 1 ist" + notebook[0]);
+console.log("Notiz 1 ist" + notebook[1]);
+console.log("Notiz 1 ist" + notebook[2]);
+console.log("Notiz 1 ist" + notebook[3]);
+console.log("Notiz 1 ist" + notebook[4]);
+console.log("Notiz 1 ist" + notebook[5]);
